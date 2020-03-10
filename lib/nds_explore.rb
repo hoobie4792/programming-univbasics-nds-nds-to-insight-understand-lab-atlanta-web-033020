150d5f480eb8f7d0c37b7cf40e21b38d03834e6d
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-10 14:51:31 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,9 +9,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  column_index = 0
-  while column_index < directors_database[0].length do
-    puts directors_database[0][column_index]
-    column_index += 1
-  end
+  
 end
